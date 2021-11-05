7513e5ba2bed8d0d90d8a406677b6ad2f5575e48
Scott Bradley <scottabradley91@gmail.com>
2021-11-05 12:37:12 -0400

    wrote code for the server.js file, will add in the routes next

---
+++ server.js
@@ -0,0 +1,26 @@
+// Server Dependencies
+const express = require("express");
+const app = express();
+
+// Port for listener to call
+let PORT = process.env.PORT || 3000;
+
+// Routers
+const apiRouter = require("./routes/apiRoutes");
+const htmlRouter = require("./routes/htmlRoutes");
+
+// For parsing data using express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware
+app.use(express.static("public"));
+app.use(express.static("db"));
+
+app.use(apiRouter);
+app.use(htmlRouter);
+
+// listener for starting the server
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+
+// run 'npm start' or 'nodemon server.js' to start up the server..
